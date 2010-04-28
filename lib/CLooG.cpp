850d48b74b042f0fd132b1fcb859b0008df1983d
Andreas Simbürger <simbuerg@fim.uni-passau.de>
2010-04-28 11:14:34 +0200

    Annotate the CloogStatement with the analyzed SCoP's BasicBlock

--- lib/CLooG.cpp
+++ lib/CLooG.cpp
@@ -93,6 +93,7 @@ void CLooG::buildCloogOptions() {
 CloogLoop *CLooG::buildCloogLoop(SCoPStmt* stmt) {
   CloogStatement *Statement = cloog_statement_malloc(State);
   Statement->number = StatementNumber++;
+  Statement->usr = stmt->getBasicBlock();
 
   CloogBlock *Block = cloog_block_alloc(Statement, 0, NULL, 1);
   CloogDomain *Domain =
