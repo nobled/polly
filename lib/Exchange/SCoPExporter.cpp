5f1ac95f8f6cf5711d26b6dc24e3269594ca4008
Tobias Grosser <grosser@fim.uni-passau.de>
2010-05-19 21:30:46 +0200

    SCoPExporter: Create .scop files

--- lib/Exchange/SCoPExporter.cpp
+++ lib/Exchange/SCoPExporter.cpp
@@ -10,7 +10,14 @@
 // Export the SCoPs build by SCoPInfo pass to text file.
 //
 //===----------------------------------------------------------------------===//
+
 #include "polly/SCoPExchange.h"
+#include "polly/SCoPInfo.h"
+
+#define OPENSCOP_INT_T_IS_MP
+#include "openscop/openscop.h"
+
+#include "stdio.h"
 
 using namespace llvm;
 using namespace polly;
@@ -19,17 +26,45 @@ namespace {
 
 struct SCoPExporter : public RegionPass {
   static char ID;
+  SCoP *S;
   explicit SCoPExporter() : RegionPass(&ID) {}
 
   virtual bool runOnRegion(Region *R, RGPassManager &RGM);
+  void getAnalysisUsage(AnalysisUsage &AU) const;
 };
 
 }
 
 char SCoPExporter::ID = 0;
 
+openscop_scop_p	ScopToOpenScop(SCoP *S) {
+  openscop_scop_p OpenSCoP = openscop_scop_malloc();
+  return OpenSCoP;
+}
+
 bool SCoPExporter::runOnRegion(Region *R, RGPassManager &RGM) {
+  S = getAnalysis<SCoPInfo>().getSCoP();
+
+  if (!S)
+    return false;
+
+  std::string exit = R->getExit() ? R->getExit()->getNameStr()
+    : "FunctionReturn";
+  std::string Filename = R->getEntry()->getParent()->getNameStr() + "-"
+    + R->getEntry()->getNameStr() + "_to_" + exit
+    + ".scop";
+  errs() << "Writing '" << Filename << "'...\n";
+
+  FILE *F = fopen(Filename.c_str(), "w");
+  openscop_scop_print_dot_scop(F, ScopToOpenScop(S));
+  fclose(F);
+
   return false;
+}
+
+void SCoPExporter::getAnalysisUsage(AnalysisUsage &AU) const {
+  AU.setPreservesAll();
+  AU.addRequired<SCoPInfo>();
 }
 
 static RegisterPass<SCoPExporter> A("polly-export-scop",
