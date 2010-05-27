367457364ff1702b84033d661b893fba3da00be4
Tobias Grosser <grosser@fim.uni-passau.de>
2010-05-27 13:55:24 +0200

    ClastCodeGeneration: Add asserts, Remove printing

--- lib/ClastCodeGeneration.cpp
+++ lib/ClastCodeGeneration.cpp
@@ -19,6 +19,8 @@
 #include "llvm/ADT/APInt.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 #include "llvm/Support/Debug.h"
+#include "llvm/Support/ErrorHandling.h"
+
 
 #define CLOOG_INT_GMP 1
 #include "cloog/cloog.h"
@@ -52,55 +54,56 @@ struct codegenctx : cp_ctx {
   unsigned assignmentCount;
 };
 
-  /// Create a loop on a specific edge
-  /// @returns An edge on which the loop body can be inserted.
-  succ_iterator createLoop(succ_iterator edge, Value *UB,
-                           Pass *P, APInt stride, Value **IV) {
-    BasicBlock *dest = *edge;
-    BasicBlock *src = edge.getSource();
-    const IntegerType *LoopIVType = IntegerType::getInt64Ty(src->getContext());
+/// Create a loop on a specific edge
+/// @returns An edge on which the loop body can be inserted.
+succ_iterator createLoop(succ_iterator edge, Value *UB,
+                         Pass *P, APInt stride, Value **IV) {
+  BasicBlock *dest = *edge;
+  BasicBlock *src = edge.getSource();
+  const IntegerType *LoopIVType = IntegerType::getInt64Ty(src->getContext());
 
-    // Create the loop header
-    BasicBlock *loop = SplitEdge(src, dest, P);
+  // Create the loop header
+  BasicBlock *loop = SplitEdge(src, dest, P);
 
-    if (*succ_begin(loop) != dest) {
-      loop = dest;
-      dest = *succ_begin(dest);
-    }
+  if (*succ_begin(loop) != dest) {
+    loop = dest;
+    dest = *succ_begin(dest);
+  }
 
-    loop->setName("polly.loop.header");
-    TerminatorInst *oldTerminator = loop->getTerminator();
-    PHINode *loopIV = PHINode::Create(LoopIVType, "polly.loopiv",
-                                      loop->begin());
-    Value *iv = static_cast<Value*>(loopIV);
-    *IV = iv;
+  loop->setName("polly.loop.header");
+  TerminatorInst *oldTerminator = loop->getTerminator();
+  PHINode *loopIV = PHINode::Create(LoopIVType, "polly.loopiv",
+                                    loop->begin());
+  Value *iv = static_cast<Value*>(loopIV);
+  *IV = iv;
 
-    ICmpInst *IS = new ICmpInst(oldTerminator, ICmpInst::ICMP_SGT, loopIV,
-                                UB);
-    BranchInst::Create(dest, loop, IS, loop);
-    loopIV->addIncoming(loopIV, loop);
-    loopIV->addIncoming(ConstantInt::get(LoopIVType, 0), src);
-    oldTerminator->eraseFromParent();
+  ICmpInst *IS = new ICmpInst(oldTerminator, ICmpInst::ICMP_SGT, loopIV,
+                              UB);
+  BranchInst::Create(dest, loop, IS, loop);
+  loopIV->addIncoming(loopIV, loop);
+  loopIV->addIncoming(ConstantInt::get(LoopIVType, 0), src);
+  oldTerminator->eraseFromParent();
 
-    // Create the loop latch
-    BasicBlock *latch = SplitEdge(loop, loop, P);
-    latch->setName("polly.loop.latch");
+  // Create the loop latch
+  BasicBlock *latch = SplitEdge(loop, loop, P);
+  latch->setName("polly.loop.latch");
 
-    // Add loop induction variable increment
-    stride.zext(64);
-    Value *ConstOne = ConstantInt::get(src->getContext(), stride);
-    Instruction* IncrementedIV = BinaryOperator::CreateAdd(loopIV, ConstOne);
-    IncrementedIV->insertBefore(latch->begin());
-    loopIV->replaceUsesOfWith(loopIV,IncrementedIV);
+  // Add loop induction variable increment
+  stride.zext(64);
+  Value *ConstOne = ConstantInt::get(src->getContext(), stride);
+  Instruction* IncrementedIV = BinaryOperator::CreateAdd(loopIV, ConstOne);
+  IncrementedIV->insertBefore(latch->begin());
+  loopIV->replaceUsesOfWith(loopIV,IncrementedIV);
 
-    // Return the loop body
-    succ_iterator SI = succ_begin(loop);
+  // Return the loop body
+  succ_iterator SI = succ_begin(loop);
 
-    while (*SI != latch)
-      ++SI;
+  while (*SI != latch)
+    ++SI;
 
-    return SI;
-   }
+  return SI;
+}
+
 succ_iterator copyBB(succ_iterator edge, BasicBlock *BB,
                      ValueMapT &VMap, Pass *P) {
   BasicBlock *dest = *edge;
@@ -134,36 +137,23 @@ succ_iterator copyBB(succ_iterator edge, BasicBlock *BB,
 
   return succ_begin(BBCopy);
 }
+
 class CPCodeGenerationActions : public CPActions {
-  private:
-    raw_ostream *ost;
-
-  void indent(int depth) {
-    for(int i=0;i<depth;i++)
-      *ost << " ";
-  }
-
   protected:
   void print(struct clast_assignment *a, int depth, cp_ctx *ctx) {
     codegenctx *cg_ctx = (codegenctx*) ctx;
     switch(ctx->dir) {
       case DFS_IN:
-        indent(depth);
-	if(a->LHS)
-	  *ost << a->LHS << "=";
 	eval(a->RHS, ctx);
 
-        if(!a->LHS) {
+        if (!a->LHS) {
           PHINode *PN = cg_ctx->stmt->IVS[cg_ctx->assignmentCount];
           cg_ctx->assignmentCount++;
           Value *V = PN;
-          if (PN->getNumOperands() == 2) {
+          if (PN->getNumOperands() == 2)
             V = *(PN->use_begin());
-          }
           cg_ctx->ValueMap[V] = cg_ctx->exprValue;
         }
-
-	*ost << "\n";
 	break;
       case DFS_OUT:
 	break;
@@ -180,14 +170,10 @@ class CPCodeGenerationActions : public CPActions {
         cg_ctx->BB = BB;
         cg_ctx->stmt = stmt;
         cg_ctx->assignmentCount = 0;
-	indent(depth);
-	*ost << BB->getNameStr() << " {\n";
 	break;
       case DFS_OUT:
         cg_ctx->edge = copyBB(cg_ctx->edge, BB, cg_ctx->ValueMap, cg_ctx->P);
         cg_ctx->BB = 0;
-	indent(depth);
-	*ost << "}\n";
 	break;
     }
   }
@@ -195,12 +181,8 @@ class CPCodeGenerationActions : public CPActions {
   void print(struct clast_block *b, int depth, cp_ctx *ctx) {
     switch(ctx->dir) {
       case DFS_IN:
-	indent(depth);
-	*ost << "{\n";
 	break;
       case DFS_OUT:
-	indent(depth);
-	*ost << "}\n";
 	break;
     }
   }
@@ -211,65 +193,36 @@ class CPCodeGenerationActions : public CPActions {
       case DFS_IN: {
 	APInt stride = APInt_from_MPZ(f->stride);
         Value *IV;
-	indent(depth);
-	*ost << "for (" << f->iterator <<"=";
 	eval(f->LB, ctx);
         //Value *LB = cg_ctx->exprValue;
-	*ost << ";";
-	*ost << f->iterator <<"<=";
 	eval(f->UB, ctx);
         Value *UB = cg_ctx->exprValue;
-	*ost << ";";
-	*ost << f->iterator << "+=";
-        stride.print(*ost, false);
         cg_ctx->edge = createLoop(cg_ctx->edge, UB, cg_ctx->P,
                                   stride, &IV);
         cg_ctx->edges.push_back(succ_begin(cg_ctx->edge.getSource()));
         CharMapT *M = &cg_ctx->CharMap;
         (*M)[f->iterator] = IV;
         cg_ctx->loop_ivs.push_back(IV);
-	*ost << ") {\n";
 	break;
       }
       case DFS_OUT:
         cg_ctx->edge = cg_ctx->edges.back();
         cg_ctx->edges.pop_back();
         cg_ctx->loop_ivs.pop_back();
-	indent(depth);
-	*ost << "}\n";
 	break;
     }
   }
 
   void print(struct clast_equation *eq, int depth, cp_ctx *ctx) {
-    eval(eq->LHS, ctx);
-    if (eq->sign == 0)
-      *ost << "==";
-    else if (eq->sign > 0)
-      *ost << ">=";
-    else
-      *ost << "<=";
-    eval(eq->RHS, ctx);
+    llvm_unreachable("Clast equation not yet supported");
   }
 
   void print(struct clast_guard *g, int depth, cp_ctx *ctx) {
+    llvm_unreachable("Clast guards not yet supported");
     switch(ctx->dir) {
       case DFS_IN:
-	indent(depth);
-	*ost << "if (";
-
-	print(&(g->eq[0]), depth, ctx);
-	if (g->n > 1)
-	  for(int i=1;i<g->n;i++) {
-	    *ost << " && ";
-	    print(&(g->eq[i]), depth, ctx);
-	  }
-
-	*ost << ") {\n";
 	break;
       case DFS_OUT:
-	indent(depth);
-	*ost << "}\n";
 	break;
     }
   }
@@ -293,14 +246,13 @@ class CPCodeGenerationActions : public CPActions {
 
   void print(clast_name *e, cp_ctx *ctx) {
     if(ctx->dir == DFS_IN) {
-      *ost << e->name;
-
       codegenctx *cg_ctx = (codegenctx*) ctx;
       CharMapT::iterator I = cg_ctx->CharMap.find(e->name);
+
       if (I != cg_ctx->CharMap.end())
         cg_ctx->exprValue = I->second;
       else
-        assert(false && "Value not found");
+        llvm_unreachable("Value not found");
 
     }
   }
@@ -312,15 +264,11 @@ class CPCodeGenerationActions : public CPActions {
       a.zext(64);
       Value *ConstOne = ConstantInt::get((cg_ctx->edge)->getContext(), a);
       cg_ctx->exprValue = ConstOne;
-      *ost << "(";
-      a.print(*ost, true);
-      *ost << ")";
-      if(e->var)
-        *ost << "*";
     }
   }
 
   void print(clast_binary *e, cp_ctx *ctx) {
+    llvm_unreachable("Binary expressions not yet supported");
     switch(ctx->dir) {
       case DFS_IN:
         break;
@@ -329,39 +277,20 @@ class CPCodeGenerationActions : public CPActions {
     }
   }
 
-  void print_red_name(clast_reduction *r, cp_ctx *ctx) {
-    switch(r->type) {
-      case clast_red_min:
-	*ost << "min(";
-	break;
-      case clast_red_max:
-	*ost << "max(";
-	break;
-      case clast_red_sum:
-	*ost << "sum(";
-    }
-  }
-
   void print(clast_reduction *r, cp_ctx *ctx) {
     if (ctx->dir == DFS_IN) {
-      const char *delim = NULL;
-      print_red_name(r, ctx);
-
-      if (r->type == clast_red_min)
-        delim = ",";
-      else if (r->type == clast_red_max)
-        delim = ",";
-      else
-        delim = "";
-
+      assert((   r->type == clast_red_min
+              || r->type == clast_red_max
+              || r->type == clast_red_sum)
+             && "Reduction type not supported");
+      assert(r->n == 1
+             && "Reductions with more than one element not supported");
       eval(r->elts[0], ctx);
-      for (int i=1; i < r->n; ++i) {
-        *ost << delim;
+
+      // TODO: Support reductions with more than one element.
+      for (int i=1; i < r->n; ++i)
         eval(r->elts[i], ctx);
-      }
     }
-    else
-      *ost << ")";
   }
 
   void print(clast_expr *e, cp_ctx *ctx) {
@@ -383,7 +312,6 @@ class CPCodeGenerationActions : public CPActions {
 
   public:
   CPCodeGenerationActions(raw_ostream& strm) {
-    ost = &strm;
   }
 
   virtual void in(clast_stmt *s, int depth, cp_ctx *ctx) {
