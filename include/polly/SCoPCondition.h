582c129e5ad8a3743fe336ecf624329f64f95491
ether <etherzhhb@gmail.com>
2010-06-02 21:19:28 +0800

    Add SCoPConditon.

---
+++ include/polly/SCoPCondition.h
@@ -0,0 +1,12 @@
+//===------ polly/SCoPCondition.h - Extract conditons in CFG -----*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// Extract conditions in CFG, so we could apply them to iterate domain.
+//
+//===----------------------------------------------------------------------===//
---
+++ lib/SCoPCondition.cpp
@@ -0,0 +1,12 @@
+//===---------- SCoPConditon.cpp  - Extract conditons in CFG ------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// Implement a pass that extrct the conditon information of CFGs.
+//
+//===----------------------------------------------------------------------===//
