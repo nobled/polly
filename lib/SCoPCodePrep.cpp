35d8cbb7198d0289df5ddcccbbc2a5d2fbafc806
ether <etherzhhb@gmail.com>
2010-08-10 13:41:10 +0800

    Fix the pass description for SCoPCodePrep pass.

--- lib/SCoPCodePrep.cpp
+++ lib/SCoPCodePrep.cpp
@@ -148,8 +148,8 @@ void SCoPCodePrep::print(raw_ostream &OS, const Module *) const {
 
 char SCoPCodePrep::ID = 0;
 
-RegisterPass<SCoPCodePrep> X("polly-scalar-data-ref",
-                              "Polly - Scalar Data Reference Analysis",
+RegisterPass<SCoPCodePrep> X("polly-code-prep",
+                              "Polly - Code preparation for SCoP detection",
                               false, true);
 
 char &polly::SCoPCodePrepID = SCoPCodePrep::ID;
