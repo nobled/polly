fea8b4d02d9a17b08f4a43a5538fbd66cfe39d6f
Tobias Grosser <grosser@fim.uni-passau.de>
2010-10-12 11:28:38 -0400

    Fix gramschidt without params

--- test/create_ll.sh
+++ test/create_ll.sh
@@ -5,6 +5,6 @@ clang -S -emit-llvm -O0 $1
 SFILE=`echo $1 | sed -e 's/\.c/.s/g'`
 LLFILE=`echo $1 | sed -e 's/\.c/.ll/g'`
 
-opt -mem2reg -indvars ${SFILE} -S > ${LLFILE}
+opt -correlated-propagation -mem2reg -loopsimplify -indvars ${SFILE} -S > ${LLFILE}
 
 rm ${SFILE}
