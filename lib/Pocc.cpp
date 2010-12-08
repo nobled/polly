b2d901fb53d738a67d30ebaacae2268414f759bf
Tobias Grosser <grosser@fim.uni-passau.de>
2010-12-08 17:19:28 -0500

    Pocc: Dump openscopfile and execute pocc

--- lib/Pocc.cpp
+++ lib/Pocc.cpp
@@ -19,6 +19,10 @@
 #include "polly/Cloog.h"
 #include "polly/LinkAllPasses.h"
 #include "polly/ScopInfo.h"
+#include "polly/ScopLib.h"
+
+#include "llvm/Support/Path.h"
+#include "llvm/Support/Program.h"
 
 using namespace llvm;
 using namespace polly;
@@ -37,6 +41,51 @@ namespace {
 }
 char Pocc::ID = 0;
 bool Pocc::runOnScop(Scop &S) {
+  // Create the scop file.
+  sys::Path tempDir = sys::Path::GetTemporaryDirectory();
+  sys::Path scopFile = tempDir;
+  scopFile.appendComponent("polly.scop");
+  scopFile.createFileOnDisk();
+
+  FILE *F = fopen(scopFile.c_str(), "w");
+
+  if (!F) {
+    errs() << "Cannot open file: " << tempDir.c_str() << "\n";
+    errs() << "Skipping export.\n";
+    return false;
+  }
+
+  ScopLib scoplib(&S);
+  scoplib.print(F);
+  fclose(F);
+
+  // Execute pocc
+  sys::Program program;
+
+  std::string poccName = "pocc";
+  sys::Path pocc = sys::Program::FindProgramByName(poccName);
+
+  std::vector<const char*> arguments;
+  arguments.push_back("pocc");
+  arguments.push_back("--read-scop");
+  arguments.push_back(scopFile.c_str());
+  arguments.push_back("--pluto-tile-scat");
+  arguments.push_back("--cloogify-scheds");
+  arguments.push_back("--output-scop");
+  arguments.push_back(0);
+
+  sys::Path plutoStdout = tempDir;
+  plutoStdout.appendComponent("pluto.stdout");
+  sys::Path plutoStderr = tempDir;
+  plutoStderr.appendComponent("pluto.stderr");
+
+  std::vector<sys::Path*> redirect;
+  redirect.push_back(0);
+  redirect.push_back(&plutoStdout);
+  redirect.push_back(&plutoStderr);
+
+  program.Execute(pocc, &arguments[0], 0, (sys::Path const **) &redirect[0]);
+
   return false;
 }
 
