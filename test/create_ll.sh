d6fd35256fde85f9741160f65c64846451f2ab42
Tobias Grosser <grosser@fim.uni-passau.de>
2010-12-10 17:59:00 -0500

    Call instnamer before generating the test cases

--- test/create_ll.sh
+++ test/create_ll.sh
@@ -5,6 +5,7 @@ clang -S -emit-llvm -O0 $1
 SFILE=`echo $1 | sed -e 's/\.c/.s/g'`
 LLFILE=`echo $1 | sed -e 's/\.c/.ll/g'`
 
-opt -correlated-propagation -mem2reg -instcombine -loopsimplify -indvars ${SFILE} -S > ${LLFILE}
+opt -correlated-propagation -mem2reg -instcombine -loopsimplify -indvars \
+-instnamer ${SFILE} -S > ${LLFILE}
 
 rm ${SFILE}
