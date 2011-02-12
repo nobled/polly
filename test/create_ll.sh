42e7977faaa73d43faed732154948cd0f89eb828
Tobias Grosser <grosser@fim.uni-passau.de>
2011-02-12 11:23:11 -0500

    Adapt to loopsimplify -> loop-simplify

--- test/create_ll.sh
+++ test/create_ll.sh
@@ -5,7 +5,7 @@ clang -S -emit-llvm -O0 $1
 SFILE=`echo $1 | sed -e 's/\.c/.s/g'`
 LLFILE=`echo $1 | sed -e 's/\.c/.ll/g'`
 
-opt -correlated-propagation -mem2reg -instcombine -loopsimplify -indvars \
+opt -correlated-propagation -mem2reg -instcombine -loop-simplify -indvars \
 -instnamer ${SFILE} -S > ${LLFILE}
 
 rm ${SFILE}
